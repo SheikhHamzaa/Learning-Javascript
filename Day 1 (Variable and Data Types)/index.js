fb7862de502238dbaff218da109e78c090f4929c
SheikhHamza <sheikhhamzanadeem77@gmail.com>
2024-07-15 03:38:19 +0500

    Day 1

---
+++ Day 1 (Variable and Data Types)/index.js
@@ -0,0 +1,34 @@
+// Define a variable with a number
+var number = 10;
+console.log(number); // Output: 10
+
+// Define a variable with a string using let
+let fruit = "apple";
+console.log(fruit); // Output: apple
+
+// Define a constant with a boolean value
+const answer = true;
+console.log(answer); // Output: true
+
+
+// variable of diffrent datatypes (num, str, bool, array, obj) and log each variable's datatypes using typeof 
+
+// Number
+let num = 42;
+console.log(`num is of type: ${typeof num}`);
+
+// String
+let str = "Hello, World!";
+console.log(`str is of type: ${typeof str}`);
+
+// Boolean
+let bool = true;
+console.log(`bool is of type: ${typeof bool}`);
+
+// Array
+let arr = [1, 2, 3];
+console.log(`arr is of type: ${typeof arr}`);
+
+// Object
+let obj = { name: "Alice", age: 30 };
+console.log(`obj is of type: ${typeof obj}`);
